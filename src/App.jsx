48902234a6313c941b3ddc6c1f4a9edca3036626
Mayank Kumar Sharma <pigamers1123@gmail.com>
2024-07-09 10:10:14 +0000

    pagination setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Products from './components/Products'
+import Pagination from './components/Pagination'
 
 export default function App() {
   return (
     <>
       <div>
         <Products />
+        <Pagination />
       </div>
     </>
   )
